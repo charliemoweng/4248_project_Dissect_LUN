f5ae335098ce895420abb56e96a58a62b64d742c
Parashara <parasharar@gmail.com>
2024-03-20 13:13:46 +0800

    Making more cosmetic changes.. core logic for pinging llm needs to be written now

--- scripts/llm_augment.py
+++ scripts/llm_augment.py
@@ -5,14 +5,69 @@ Steps to run:
 
 1. Download dataset from https://github.com/BUPT-GAMMA/CompareNet_FakeNewsDetection/releases/tag/dataset in case its not already done
 2. Save it in a directory called dataset (but don't commit the dataset)
-3. Run the script using your runid
-
+3. Open fulltrain.csv and add this in the first line
+"Label", "Text"
+4. Run the script using your runid
 python llm_augment.py --runid {1-6}
+5. This will create a csv files ../dataset/{your_run_id)_train_aug.csv
+
+
 '''
 
 import json
 import requests
+import pandas as pd
+from tqdm import tqdm
+import argparse
 
+'''
+Current Class counts are:
+
+Satire(1)           14047
+Hoax(2)             6942
+Propaganda(3)       17870
+Reliable News(4)    9995
+
+Goal is to get Hoax(2) and Reliable News(4) to around 14000
+'''
+
+class_to_label_mapper = {
+    1: "Satire", 2: "Hoax", 3: "Propaganda", 4: "Reliable News"
+}
+
+label_to_class_mapper = {
+    "Satire": 1, "Hoax": 2, "Propaganda": 3, "Reliable News": 4
+}
+
+
+def split_dataset(dataset):
+    '''
+    Goal is to save 6 different datasets with equal split of Hoax and Reliable News subsets as that is the split we want to increase
+
+    :param dataset:
+    :return:
+    '''
+    num_splits = 6
+    counts = dataset["Label"].value_counts()
+
+    # split hoax 6 ways
+    hoax = dataset[dataset["Label"] == label_to_class_mapper["Hoax"]]
+    hoax_split_size = len(hoax) // num_splits
+    hoax_splits = [hoax[i * hoax_split_size:(i + 1) * hoax_split_size] for i in range(num_splits)]
+
+    reliable = dataset[dataset["Label"] == label_to_class_mapper["Reliable News"]]
+    reliable_split_size = len(reliable) // num_splits
+    reliable_splits = [reliable[i * reliable_split_size:(i + 1) * reliable_split_size] for i in range(num_splits)]
+
+    # saving the splits
+    for i in range(num_splits):
+        # Concatenate A_i and B_i
+        combined_df = pd.concat([hoax_splits[i], reliable_splits[i]])
+
+        # Save the split in the datasets folder
+        combined_df.to_csv(f'../dataset/{i + 1}_train.csv', index=False)
+
+        print(f'Saved {i + 1}_train.csv')
 
 def ping_llm():
     prompt = """
@@ -31,25 +86,61 @@ def ping_llm():
     
     Paraphrase the following:
     """
-    try:
+    # try:
+    #
+    #     example = "{SOS}The writers of the HBO series The Sopranos took another daring storytelling step by killing off 10 million fans during the seventh season's premiere episode Sunday night. 'This was definitely a bold choice, one that producers of the show would have never thought of making five years ago,' said New York Times television critic Virginia Heffernan, who noted that the move was hinted at in a season-five episode in which Tony dreamt he was riding a horse through his house. 'But now that I look back, this was strongly foreshadowed throughout all of last season.' Industry insiders predicted that the show's producers would try to bring at least some fans back for the series finale, which may come as early as May. {SOS}|{CL}Satire{CL}"
+    #
+    #     url = "https://iiced-mixtral-46-7b-fastapi.hf.space/generate/"
+    #     data = {
+    #         "prompt": prompt + example,
+    #         "history": [],
+    #         "system_prompt": "You are expected to help in augmenting a text based dataset for 4-way text classification"
+    #     }
+    #     headers = {'Content-Type': 'application/json'}
+    #
+    #     response = requests.post(url, data=json.dumps(data), headers=headers)
+    #     return response.json()
+    # except Exception as e:
+    #     print(f"Failed with exception {e}")
+    #     return
 
-        example = "{SOS}The writers of the HBO series The Sopranos took another daring storytelling step by killing off 10 million fans during the seventh season's premiere episode Sunday night. 'This was definitely a bold choice, one that producers of the show would have never thought of making five years ago,' said New York Times television critic Virginia Heffernan, who noted that the move was hinted at in a season-five episode in which Tony dreamt he was riding a horse through his house. 'But now that I look back, this was strongly foreshadowed throughout all of last season.' Industry insiders predicted that the show's producers would try to bring at least some fans back for the series finale, which may come as early as May. {SOS}|{CL}Satire{CL}"
-
-        url = "https://iiced-mixtral-46-7b-fastapi.hf.space/generate/"
-        data = {
-            "prompt": prompt+example,
-            "history": [],
-            "system_prompt": "You are expected to help in augmenting a text based dataset for 4-way text classification"
-        }
-        headers = {'Content-Type': 'application/json'}
-
-        response = requests.post(url, data=json.dumps(data), headers=headers)
-        return response.json()
-    except Exception as e:
-        print(f"Failed with exception {e}")
-        return
-
+    return
 
 if __name__ == '__main__':
-    response = ping_llm()
-    print(response.json())
+    dataset = pd.read_csv("../dataset/fulltrain.csv")
+
+    # run this in case you still want to split it (it will most likely be the same thing)
+    # split_dataset(dataset)
+
+    label_counts = dataset["Label"].value_counts()
+
+    no_of_hoax_to_generate_per_person = (label_counts[label_to_class_mapper["Satire"]] - label_counts[
+        label_to_class_mapper["Hoax"]]) // 6
+    no_of_reliable_to_generate_per_person = (label_counts[label_to_class_mapper["Satire"]] - label_counts[
+        label_to_class_mapper["Reliable News"]]) // 6
+
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Script with --runid argument")
+
+    # Add the --runid argument
+    parser.add_argument('--runid', type=int, choices=range(1, 7), help="Specify a number between 1 and 6")
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Check if the --runid argument was provided
+    if args.runid is None:
+        parser.print_help()
+        exit(1)
+
+    # Access the value of --runid argument
+    run_id = args.runid
+
+    print(f"Reading the split dataset ../dataset/{run_id}_train.csv")
+    split_dataset = pd.read_csv(f"../dataset/{run_id}_train.csv")
+
+    print(f"Need to generate {no_of_reliable_to_generate_per_person} reliable texts & {no_of_hoax_to_generate_per_person} hoax texts")
+    hoax_generations = []
+    reliable_generations = []
+
+    #TODO. now write the code to batch it into llm query and try threading as well !
